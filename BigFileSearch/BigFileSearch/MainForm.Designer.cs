1978bdb0ee0cb0041934753a2f48ba9ef2f4a922
Lawrence <yangfei6565@163.com>
2015-07-11 19:33:57 +0800

    加入了 search 方法

---
+++ BigFileSearch/BigFileSearch/MainForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace BigFileSearch
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchProgressBar = new System.Windows.Forms.ProgressBar();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressTimer = new System.Windows.Forms.Timer(this.components);
+            this.searchResultListView = new System.Windows.Forms.ListView();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.stopSearchButton = new System.Windows.Forms.Button();
+            this.文件名 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.大小 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(830, 36);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(85, 27);
+            this.searchButton.TabIndex = 0;
+            this.searchButton.Text = "马上搜索";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButtonTouched);
+            // 
+            // searchProgressBar
+            // 
+            this.searchProgressBar.Location = new System.Drawing.Point(26, 112);
+            this.searchProgressBar.Name = "searchProgressBar";
+            this.searchProgressBar.Size = new System.Drawing.Size(889, 23);
+            this.searchProgressBar.TabIndex = 1;
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.folderBrowserDialog.HelpRequest += new System.EventHandler(this.folderBrowserDialog_HelpRequest);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "搜索路径";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(83, 40);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(628, 21);
+            this.searchTextBox.TabIndex = 4;
+            this.searchTextBox.TextChanged += new System.EventHandler(this.searchTextBox_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(728, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 27);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "选择路径";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.selectDirectoryButton);
+            // 
+            // progressTimer
+            // 
+            this.progressTimer.Tick += new System.EventHandler(this.progressTimer_Tick);
+            // 
+            // searchResultListView
+            // 
+            this.searchResultListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.文件名,
+            this.大小});
+            this.searchResultListView.Location = new System.Drawing.Point(26, 141);
+            this.searchResultListView.Name = "searchResultListView";
+            this.searchResultListView.Size = new System.Drawing.Size(889, 293);
+            this.searchResultListView.TabIndex = 6;
+            this.searchResultListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("黑体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.timeLabel.Location = new System.Drawing.Point(22, 88);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(65, 21);
+            this.timeLabel.TabIndex = 7;
+            this.timeLabel.Text = "00:00";
+            // 
+            // stopSearchButton
+            // 
+            this.stopSearchButton.Location = new System.Drawing.Point(830, 69);
+            this.stopSearchButton.Name = "stopSearchButton";
+            this.stopSearchButton.Size = new System.Drawing.Size(85, 23);
+            this.stopSearchButton.TabIndex = 8;
+            this.stopSearchButton.Text = "停止搜索";
+            this.stopSearchButton.UseVisualStyleBackColor = true;
+            this.stopSearchButton.Click += new System.EventHandler(this.stopSearchButton_Click_1);
+            // 
+            // 大小
+            // 
+            this.大小.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 446);
+            this.Controls.Add(this.stopSearchButton);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.searchResultListView);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchProgressBar);
+            this.Controls.Add(this.searchButton);
+            this.Name = "MainForm";
+            this.Text = "Big File Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.ProgressBar searchProgressBar;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer progressTimer;
+        private System.Windows.Forms.ListView searchResultListView;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Button stopSearchButton;
+        private System.Windows.Forms.ColumnHeader 文件名;
+        private System.Windows.Forms.ColumnHeader 大小;
+
+    }
+}
+
---
+++ BigFileSearch/BigFileSearch/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace BigFileSearch
+{
+    public partial class MainForm : Form
+    {
+        private List<FileInfo> searchResult = new List<FileInfo>();
+        private DateTime start;
+        private string searchCriteria = "";
+        public MainForm()
+        {
+            InitializeComponent();
+            // 初始化
+            stopSearchButton.Enabled = false;
+        }
+
+        private void searchButtonTouched(object sender, EventArgs e)
+        {
+            // 准备搜索
+            start = DateTime.Now;
+            progressTimer.Enabled = true;
+            progressTimer.Start();
+            stopSearchButton.Enabled = true;
+
+            // 搜索
+            searchResult.Clear();
+            startSearch();
+            
+        }
+
+        private void startSearch()
+        {
+            var dir = new DirectoryInfo(searchCriteria);
+            searchResult = dir.GetFiles().ToList();
+        }
+        private void stopSearchButton_Click_1(object sender, EventArgs e)
+        {
+            progressTimer.Enabled = false;
+            progressTimer.Stop();
+            stopSearchButton.Enabled = false;
+        }
+
+        private void progressTimer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan elapsed = DateTime.Now - start;
+            timeLabel.Text = String.Format("Total time: {0:hh\\:mm\\:ss}", elapsed);
+        }
+
+        private void folderBrowserDialog_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectDirectoryButton(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+
+            if (dlg.ShowDialog(this) == DialogResult.OK)
+            {
+                if (!dlg.SelectedPath.Equals(""))
+                {
+                    searchTextBox.Text = dlg.SelectedPath;
+                }
+            }	
+        }
+
+        private void searchTextBox_TextChanged(object sender, EventArgs e)
+        {
+            searchCriteria = searchTextBox.Text;
+        }
+
+    }
+}
